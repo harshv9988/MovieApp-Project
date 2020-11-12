1ae96337145ccbe11a1f6c6b57375c61eb685175
harshv9988 <harshv9988@gmail.com>
2020-11-12 13:12:56 +0530

    added action reducer dynamic functinality

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,6 +1,16 @@
-export default function movies(state = [], action) {
-  if (action.type == "ADD_MOVIES") {
-    return action.movies;
+import { ADD_MOVIES } from "../Actions/index";
+
+const initialState = {
+  list: [],
+  favourites: [],
+};
+
+export default function movies(state = initialState, action) {
+  if (action.type === ADD_MOVIES) {
+    return {
+      ...state,
+      list: action.movies,
+    };
   }
   return state;
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../Actions/index";
 
 class App extends React.Component {
   componentDidMount() {
@@ -10,13 +11,10 @@ class App extends React.Component {
       this.forceUpdate();
     });
 
-    store.dispatch({
-      type: "ADD_MOVIES",
-      movies: data,
-    });
+    store.dispatch(addMovies(data));
   }
   render() {
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState();
     return (
       <div className="App">
         <Navbar />
@@ -26,8 +24,8 @@ class App extends React.Component {
             <div className="tab">Favourites</div>
           </div>
           <div className="list">
-            {movies.map((movie) => {
-              return <MovieCard movie={movie} />;
+            {list.map((movie, index) => {
+              return <MovieCard movie={movie} key={index} />;
             })}
           </div>
         </div>
